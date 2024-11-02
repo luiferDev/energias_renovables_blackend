b674ae9a0de5716fab41ba3f52ebd52ad4396a6f
Jorge Morales <luifer991@gmail.com>
2024-11-01 23:57:58 -0500

    coneccion con las tablas planta produccion y energia soar

--- src/main/java/com/energias/renovables/modelo/energiasrenovables/EnergiasRenovables.java
+++ src/main/java/com/energias/renovables/modelo/energiasrenovables/EnergiasRenovables.java
@@ -1,7 +1,10 @@
 package com.energias.renovables.modelo.energiasrenovables;
 
+import com.energias.renovables.modelo.energiasolar.EnergiaSolar;
+import com.energias.renovables.modelo.plantaproduccion.PlantaProduccion;
 import com.energias.renovables.modelo.tipoenergia.TipoEnergia;
 import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,8 +21,15 @@ public class EnergiasRenovables {
     @Id
     private int id;
     private String nombre;
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "tipoenergiaid")
+    @ManyToOne ( fetch = FetchType.LAZY )
+    @JoinColumn ( name = "tipoenergiaid" )
     private TipoEnergia tipoEnergiaId;
+    
+    @MapsId
+    @OneToOne ( fetch = FetchType.LAZY )
+    @JoinColumn ( name = "id" )
+    private PlantaProduccion plantaProduccion;
+    @OneToOne ( mappedBy = "energiasRenovables", cascade = CascadeType.ALL, fetch = FetchType.LAZY )
+    private EnergiaSolar energiaSolar;
     
 }
