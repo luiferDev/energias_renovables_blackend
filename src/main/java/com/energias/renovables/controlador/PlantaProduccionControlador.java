249604449dc31b3234616dc5395bd3111ff09cff
Jorge Morales <luifer991@gmail.com>
2024-11-01 23:58:46 -0500

    controlador de la planta de produccion

---
+++ src/main/java/com/energias/renovables/controlador/PlantaProduccionControlador.java
@@ -0,0 +1,32 @@
+package com.energias.renovables.controlador;
+
+import com.energias.renovables.modelo.plantaproduccion.PlantaProduccion;
+import com.energias.renovables.modelo.plantaproduccion.PlantaProduccionRepository;
+import com.energias.renovables.modelo.plantaproduccion.PlantaProduccionService;
+import com.energias.renovables.modelo.plantaproduccion.PlantaProduccionSolarDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping ( "api/planta-produccion" )
+public class PlantaProduccionControlador {
+    
+    private PlantaProduccionRepository plantaProduccionRepository;
+    
+    public PlantaProduccionControlador ( PlantaProduccionRepository plantaProduccionRepository ) {
+        this.plantaProduccionRepository = plantaProduccionRepository;
+    }
+    @Autowired
+    PlantaProduccionService plantaProduccionService;
+    
+    @GetMapping
+    public ResponseEntity <List <PlantaProduccionSolarDTO>> getPlantaProduccion () {
+        return ResponseEntity.ok( plantaProduccionService.findAllPlantaProduccionSolar() );
+    }
+    
+}
