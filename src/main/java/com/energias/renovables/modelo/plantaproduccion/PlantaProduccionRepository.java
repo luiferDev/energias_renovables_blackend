4e1688c94c7a22010747d963a3fa2c249d1e48ba
Jorge Morales <luifer991@gmail.com>
2024-11-01 23:59:14 -0500

    repositorio de la planta de produccion

---
+++ src/main/java/com/energias/renovables/modelo/plantaproduccion/PlantaProduccionRepository.java
@@ -0,0 +1,40 @@
+package com.energias.renovables.modelo.plantaproduccion;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface PlantaProduccionRepository extends JpaRepository <PlantaProduccion, Integer> {
+//    @Query ( "SELECT p  p.id, e e.nombre AS codigo, p.ubicacion, p.capacidad_instalada, p.eficiencia," +
+//            " t t.nombre AS tipo_energia, s.radiacion_solar_promedio AS radiacion_solar" +
+//            "s s.area_paneles, s.angulo_inclinacion, p.fecha_creacion" +
+//            "FROM plantaproduccion p" +
+//            "JOIN energiasrenovables e ON e.id = p.id" +
+//            "JOIN tipoenergia t ON t.id = e.tipoenergiaid" +
+//            "JOIN energiasolar s ON s.id = e.id" )
+//    @Query( """
+//            SELECT p, e, t, s FROM PlantaProduccion p
+//            JOIN p.EnergiasRenovables e
+//            JOIN e.TipoEnergia t
+//            JOIN p.EnergiaSolar s
+//            """)
+//@Query("""
+//            SELECT new com.energias.renovables.dto.PlantaProduccionSolarDTO(
+//                p.id, e.nombre, p.ubicacion, p.capacidadInstalada, p.eficiencia,
+//                t.nombre, s.radiacionSolarPromedio, s.areaPaneles, s.anguloInclinacion, p.fechaCreacion)
+//            FROM PlantaProduccion p
+//            JOIN p.EnergiasRenovables e
+//            JOIN e.TipoEnergia t
+//            JOIN e.EnergiaSolar s
+//            """)
+//    List<PlantaProduccionSolarDTO> findAllPlantaProduccionSolar ();
+    
+    @Query("""
+        SELECT p, e, s FROM PlantaProduccion p
+        JOIN EnergiasRenovables e ON e.id = p.id
+        JOIN EnergiaSolar s ON s.id = e.id
+        """)
+    List<Object[]> findAllPlantaProduccionSolarWithEntities();
+    
+}
