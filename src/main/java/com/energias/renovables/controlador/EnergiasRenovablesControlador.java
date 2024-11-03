05499650b9cda4401d3bab9d4565f1b066a161f6
Jorge Morales <luifer991@gmail.com>
2024-11-02 23:42:47 -0500

    calculo de las energias renovables

---
+++ src/main/java/com/energias/renovables/controlador/EnergiasRenovablesControlador.java
@@ -0,0 +1,34 @@
+package com.energias.renovables.controlador;
+
+import com.energias.renovables.modelo.energiasrenovables.EnergiaRenovableFactoryService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping ( "/api/energias-renovables" )
+public class EnergiasRenovablesControlador {
+    private final EnergiaRenovableFactoryService energiaRenovableFactoryService;
+    
+    public EnergiasRenovablesControlador ( EnergiaRenovableFactoryService energiaRenovableFactoryService ) {
+        this.energiaRenovableFactoryService = energiaRenovableFactoryService;
+    }
+    
+//    @GetMapping ( "/calcular-total" )
+//    public ResponseEntity <BigDecimal> calcularProduccionEnergia ( @RequestParam String tipoenergia ) {
+//        var s = energiaRenovableFactoryService.getCalculadoraEnergia( tipoenergia )
+//                .calcularProduccionEnergia();
+//        return ResponseEntity.ok( s );
+//    }
+//
+    @GetMapping ( "/calcular" )
+    public ResponseEntity <BigDecimal> calcularProduccionEnergia ( @RequestParam String tipoenergia,
+                                                                  @RequestParam int id ) {
+        var s = energiaRenovableFactoryService.calcularProduccionPorTipo( tipoenergia, id );
+        return ResponseEntity.ok( s );
+    }
+}
