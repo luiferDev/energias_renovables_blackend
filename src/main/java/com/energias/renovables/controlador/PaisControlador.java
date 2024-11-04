dd04d144220933e9e4670948f44a27ec50b48850
Jorge Morales <luifer991@gmail.com>
2024-11-03 22:33:38 -0500

    mejorar el endpont y agregar la biomasa

--- src/main/java/com/energias/renovables/controlador/PaisControlador.java
+++ src/main/java/com/energias/renovables/controlador/PaisControlador.java
@@ -1,13 +1,12 @@
 package com.energias.renovables.controlador;
 
+import com.energias.renovables.modelo.pais.PaisPlantaEnergiaBiomasa;
 import com.energias.renovables.modelo.pais.PaisPlantaEnergiaSolarDTO;
-import com.energias.renovables.modelo.pais.PaisRepository;
 import com.energias.renovables.modelo.pais.PaisService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,9 +18,13 @@ public class PaisControlador {
     @Autowired
     PaisService paisService;
     
-    @GetMapping
-    public ResponseEntity <List <PaisPlantaEnergiaSolarDTO>> getPais () {
+    @GetMapping ( "energia-solar" )
+    public ResponseEntity <List <PaisPlantaEnergiaSolarDTO>> getPaisEnergiaSolar () {
         return ResponseEntity.ok( paisService.findAllPaisPlantaEnergiaSolar() );
     }
     
+    @GetMapping ( "biomasa" )
+    public ResponseEntity <List <PaisPlantaEnergiaBiomasa>> getPaisBiomasa () {
+        return ResponseEntity.ok( paisService.findAllPaisPlantaEnergiaBiomasa() );
+    }
 }
