63ebbee7f2932a39acfd3251cbb5f2c9e8f270c5
Jorge Morales <luifer991@gmail.com>
2024-11-04 22:45:09 -0500

    crear y borrar

--- src/main/java/com/energias/renovables/modelo/energiasolar/EnergiaSolarRepository.java
+++ src/main/java/com/energias/renovables/modelo/energiasolar/EnergiaSolarRepository.java
@@ -1,6 +1,26 @@
 package com.energias.renovables.modelo.energiasolar;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.query.Procedure;
+
+import java.math.BigDecimal;
+import java.sql.Date;
 
 public interface EnergiaSolarRepository extends JpaRepository <EnergiaSolar, Integer> {
+    
+    @Procedure ( procedureName = "insertar_energia_solar" )
+    void insertEnergiaSolar ( BigDecimal radiacionSolarPromedio,
+                              BigDecimal areaPaneles,
+                              BigDecimal anguloInclinacion,
+                              String ubicacion,
+                              String s, 
+                              BigDecimal capacidadInstalada,
+                              BigDecimal eficiencia,
+                              Date time, String nombrePais,
+                              BigDecimal energia,
+                              BigDecimal nivel,
+                              BigDecimal poblacion );
+    
+    @Procedure ( procedureName = "borrar_energia_solar" )
+    void deleteEnergiaSolar ( Integer id );
 }
